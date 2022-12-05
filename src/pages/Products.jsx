f5edaec0d2ddd7a13ad2ce08ed9b8db3336dc019
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-12-05 00:32:16 -0600

    add header product & starting div product

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,31 @@
-import Left from '../assets/svg/arrowleft.svg'
-import Right from '../assets/svg/arrowright.svg'
-import '../css/Products.css'
+import "../css/Products.css";
 
 export default function Products() {
     return (
-        <div className='div-product'>
+        <div className="div-product">
             <div className="header-product">
                 <p className="header-title">Yeni Gelen Ürünler</p>
                 <div className="header-return">
-                    <img src={Left} alt="left" className='svg-arrow' />
-                    <img src={Right} alt="right" className='svg-arrow' />
+                    <span class="material-symbols-outlined svg-arrow">
+                        chevron_left
+                    </span>
+                    <span class="material-symbols-outlined svg-arrow">
+                        chevron_right
+                    </span>
+                </div>
+            </div>
+            <div>
+                <div className="product">
+                    <div className="img-product"></div>
+                    <div className="info-product">
+                        <div className="data-product">
+                            <p className="name-product">
+                                Nike Blazer Young 2022
+                            </p>
+                            <p className="brand-product">Günlük Ayakkabi</p>
+                        </div>
+                        <p className="price-product">1.250₺</p>
+                    </div>
                 </div>
             </div>
         </div>
